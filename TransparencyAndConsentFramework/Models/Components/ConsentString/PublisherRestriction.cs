563630724104319ba352a6bcfa18bea6f581c24f
Marcos Lopez <marcos@bidtellect.com>
2021-10-13 17:50:42 -0400

    Adding code documentation

--- TransparencyAndConsentFramework/Models/Components/ConsentString/PublisherRestriction.cs
+++ TransparencyAndConsentFramework/Models/Components/ConsentString/PublisherRestriction.cs
@@ -2,17 +2,28 @@
 
 namespace Bidtellect.Tcf.Models.Components.ConsentString
 {
+    /// <summary>
+    /// Represents a TC String publisher restriction.
+    /// </summary>
     public class PublisherRestriction
     {
+        /// <summary>
+        /// Gets or sets the restriction type.
+        /// </summary>
         public RestrictionType RestrictionType { get; set; }
 
         /// <summary>
-        /// Gets or sets the purpose.
+        /// Gets or sets the Vendor's declared Purpose that the publisher has indicated that they are overriding.
         /// </summary>
+        /// <remarks>
+        /// <b>Important:</b> This value will be <c>null</c> if the parser does not
+        /// have a <c>VendorList</c> when parsing a TC String.
+        /// </remarks>
         public Purpose Purpose { get; set; }
 
         /// <summary>
-        /// Gets or sets the vendors.
+        /// Gets or sets a collection of vendors which the publisher has designated as restricted under the
+        /// Purpose in this <c>PublisherRestriction</c>.
         /// </summary>
         public VendorCollection Vendors { get; set; }
     }
--- TransparencyAndConsentFramework/Models/Components/ConsentString/RestrictionType.cs
+++ TransparencyAndConsentFramework/Models/Components/ConsentString/RestrictionType.cs
@@ -1,10 +1,28 @@
 ﻿namespace Bidtellect.Tcf.Models.Components.ConsentString
 {
+    /// <summary>
+    /// Specifies constants that define a publisher restriction.
+    /// </summary>
     public enum RestrictionType
     {
+        /// <summary>
+        /// Purpose Flatly Not Allowed by Publisher (regardless of Vendor declarations).
+        /// </summary>
         PurposeFlatlyNotAllowedByPublisher = 0,
+
+        /// <summary>
+        /// Require Consent (if Vendor has declared the Purpose IDs legal basis as Legitimate Interest and flexible).
+        /// </summary>
         RequireConsent = 1,
+
+        /// <summary>
+        /// Require Legitimate Interest (if Vendor has declared the Purpose IDs legal basis as Consent and flexible).
+        /// </summary>
         RequireLegitimateInterest = 2,
+
+        /// <summary>
+        /// UNDEFINED (not used).
+        /// </summary>
         Undefined = 3,
     }
 }
