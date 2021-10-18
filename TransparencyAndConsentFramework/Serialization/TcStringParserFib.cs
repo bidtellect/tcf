d58fb5e88fd0c1dfd5ebf197fe59e4cea6a01034
Marcos Lopez <marcos@bidtellect.com>
2021-10-18 10:07:47 -0400

    Adding Fib code

---
+++ TransparencyAndConsentFramework/Serialization/TcStringParserFib.cs
@@ -0,0 +1,76 @@
+﻿using Bidtellect.Tcf.Models;
+using Bidtellect.Tcf.Models.Components.VendorList;
+
+namespace Bidtellect.Tcf.Serialization
+{
+    /// <inheritdoc cref="TcStringParser"/>
+    public class TcStringParserFib : TcStringParser
+    {
+        protected const int MaxVendorId = (1 << 16) - 1;
+
+        /// <inheritdoc cref="TcStringParser.TcStringParser(VendorList)"/>
+        public TcStringParserFib(VendorList vendorList)
+            : base(vendorList)
+        {
+        }
+
+        protected override int ReadVersion(BitReader reader)
+        {
+            var version = reader.ReadInt(6);
+
+            if (version != 3)
+            {
+                throw new TcStringParserException(TcStringParserException.ExceptionType.InvalidVersion);
+            }
+
+            return version;
+        }
+
+        protected override int ReadVendorId(BitReader reader)
+        {
+            var value = reader.ReadFib();
+
+            if (value > MaxVendorId)
+            {
+                throw new System.InvalidOperationException();
+            }
+
+            return (int)value;
+        }
+
+        protected override int ReadVendorRangeCount(BitReader reader)
+        {
+            return (int)reader.ReadFib() - 1;
+        }
+
+        protected override void ReadVendorRange(BitReader reader, VendorCollection collection)
+        {
+            var rangeCount = ReadVendorRangeCount(reader);
+
+            var offset = 0;
+
+            for (var i = 0; i < rangeCount; i += 1)
+            {
+                var isRange = reader.ReadBit();
+
+                if (isRange)
+                {
+                    var startVendorId = ReadVendorId(reader) + offset;
+                    var endVendorId = ReadVendorId(reader) + startVendorId;
+
+                    AddVendorRange(collection, startVendorId, endVendorId);
+
+                    offset = endVendorId;
+                }
+                else
+                {
+                    var vendorId = ReadVendorId(reader) + offset;
+
+                    AddVendor(collection, vendorId);
+
+                    offset = vendorId;
+                }
+            }
+        }
+    }
+}
---
+++ TransparencyAndConsentFramework/Serialization/TcStringSerializerFib.cs
@@ -0,0 +1,42 @@
+﻿namespace Bidtellect.Tcf.Serialization
+{
+    /// <inheritdoc cref="TcStringSerializer" />
+    public class TcStringSerializerFib : TcStringSerializer
+    {
+        protected override void WriteVendorId(BitWriter writer, int vendorId)
+        {
+            writer.WriteFib(vendorId);
+        }
+
+        protected override void WriteVendorRangeCount(BitWriter writer, int count)
+        {
+            writer.WriteFib(count + 1);
+        }
+
+        protected override void WriteVendorRange(BitWriter writer, int[] orderedVendorIds)
+        {
+            var ranges = GetVendorRanges(orderedVendorIds);
+
+            WriteVendorRangeCount(writer, ranges.Count);
+
+            var lastVendorId = 0;
+
+            foreach (var range in ranges)
+            {
+                if (range.EndVendorId == range.StartVendorId)
+                {
+                    Write(writer, false);
+                    writer.WriteFib(range.StartVendorId - lastVendorId);
+                }
+                else
+                {
+                    Write(writer, true);
+                    writer.WriteFib(range.StartVendorId - lastVendorId);
+                    writer.WriteFib(range.EndVendorId - range.StartVendorId);
+                }
+
+                lastVendorId = range.EndVendorId;
+            }
+        }
+    }
+}
